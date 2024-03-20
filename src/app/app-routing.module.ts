0c8699989756c17c9bcda8e9e38d64b2b98185fb
teo.mendonca1510@gmail.com <teo.mendonca1510@gmail.com>
2024-03-19 21:19:50 -0300

    project start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component: HomeComponent},
+  {path:'login',component: LoginComponent},
+  {path:'user-register',component: UserRegisterComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
